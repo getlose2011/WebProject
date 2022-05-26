c6d39653538638ab764c32b22d7a80a7ff2c6a79
jenhsiang <getlose2011@gmail.com>
2022-05-26 17:22:19 +0800

    csrf

---
+++ WebProject/Controllers/CsrfController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebProject.Controllers
+{
+    public class CsrfController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(string first_name)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AjaxCreate()
+        {
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult AjaxCreate(string date)
+        {
+            return PartialView("_IndexDataList");
+        }
+
+    }
+}
